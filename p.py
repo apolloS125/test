6c89fff5054efe33bc0e2d2b85a13201e969c15b
Jetesada Kaewmanee <lo6ster6@gmail.com>
2023-12-02 18:37:12 +0700

    sss

--- p.py
+++ p.py
@@ -1,3 +1,3 @@
-n = int(input('Enter the number of rows: '))
+n = int(input('Input your number: '))
 for i in range(1,n+1):
     print('*'*i)
--- p6.py
+++ p6.py
@@ -1,10 +1,15 @@
 n = int(input('Enter the number of rows: '))
 symbo = input('Enter print symbol: ')
-for x in range(n,0,-1):
-  for y in range(n - x):
-    print (" ",end="")
-  for y in range(x,0,-1):
-    print(symbo,end="")
-  for y in range(x,1,-1):
-    print(symbo,end="")
-  print()
+
+
+def main(n,symbo):
+  for x in range(n,0,-1):
+    for y in range(n - x):
+      print (" ",end="")
+    for y in range(x,0,-1):
+      print(symbo,end="")
+    for y in range(x,1,-1):
+      print(symbo,end="")
+    print()
+
+main(n,symbo)
