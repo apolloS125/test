a9e80a290d2d6351f8eed83a6ff9309edf8d638c
Jetesada Kaewmanee <lo6ster6@gmail.com>
2023-12-06 15:48:00 +0700

    test2

--- Temp.py
+++ Temp.py
@@ -1,5 +1,5 @@
-c = float(input('Type in a temperature in Celsius: '))
+c2 = float(input('Type in a temperature in Celsius: '))
 
-Far = (c*9)/5+32
+Far = (c2*9)/5+32
 
 print(f'That is {Far} degrees Fahrenheit')
